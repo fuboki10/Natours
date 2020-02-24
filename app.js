e44754850b406e5617bdda5f314ec18e9445fb32
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-02-24 23:44:34 +0200

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+const x = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: x
+    }
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
