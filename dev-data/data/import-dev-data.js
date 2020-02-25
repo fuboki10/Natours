b2c9b38d54aa4a6829035fa9e6069aa8ee18f71b
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-02-25 20:30:50 +0200

    Add error handlers

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel');
 
-dotenv.config({
-  path: './config.env'
-});
+dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+    useFindAndModify: false
   })
   .then(() => console.log('DB connection successful!'));
 
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
 
+  if (process.env.NODE_ENV == 'production')
+    console.log(err.name, err.message);
+  else
+    console.log(err);
+
+  process.exit(1);
+});
+
+dotenv.config({
+  path: './config.env'
+});
 const app = require('./app');
 
 const DB = process.env.DATABASE_LOCAL;
@@ -17,6 +29,19 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+
+  if (process.env.NODE_ENV == 'production')
+    console.log(err.name, err.message);
+  else
+    console.log(err);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
