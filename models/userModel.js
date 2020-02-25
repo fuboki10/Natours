a39cc6d5efda71c7f911ac60aa2141b3bd8df1a6
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-02-25 21:47:53 +0200

    add bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,18 @@
 const mongooose = require('mongoose');
 const validator = require('validator');
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 // name email photo password passwordConfirm
 
 const userSchema = mongooose.Schema({
   name: {
     type: String,
-    require: [true, 'Please enter your name!'],
+    required: [true, 'Please enter your name!'],
     trim: true
   },
   email: {
     type: String,
-    require: [true, 'Please enter your email!'],
+    required: [true, 'Please enter your email!'],
     unique: [true, 'This email is used!'],
     lowercase: true,
     validate: [validator.isEmail, 'Please enter a valid email!']
@@ -22,12 +22,12 @@ const userSchema = mongooose.Schema({
   },
   password: {
     type: String,
-    require: [true, 'Please enter your password!'],
+    required: [true, 'Please enter your password!'],
     minlength: 8
   },
   passwordConfirm: {
     type: String,
-    require: [true, 'Please confirm your password!'],
+    required: [true, 'Please confirm your password!'],
     validate: {
       // this only works on CREATE and SAVE!!!!
       validator: function (el) {
@@ -38,8 +38,13 @@ const userSchema = mongooose.Schema({
   }
 });
 
-userSchema.pre('save', function (next) {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongooose.model('User', userSchema);
