e247e43b2e8fbe1c117b3319e671c11e8042f3c0
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-02-29 22:16:21 +0200

    Add tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,16 +4,26 @@ const catchAsync = require('../utils/catchAsync');
 exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
 
-
-
   res.status(200).render('overview', {
     title: 'All tours',
     tours
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({
+    slug: req.params.slug
+  }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour
+  });
+});
